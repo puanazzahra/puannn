63182ff4a9027c58692e7b5267b476b8b58de9e1
puanazzahra <puan.az.2203226@students.um.ac.id>
2024-09-18 10:24:23 +0700

    Update puann.py

--- puann.py
+++ puann.py
@@ -4,6 +4,7 @@ import streamlit as st
  
 # Header
 st.header('Puan :sparkles:')
+st.header('220322604060')
 st.subheader('Plot')
  
 x = np.linspace(-2 * np.pi, 2 * np.pi, 1000)  # Generating x values from -2*pi to 2*pi
