204c404f18a9f47a763145c1969d5c8f182f8ba9
puanazzahra <puan.az.2203226@students.um.ac.id>
2024-09-18 10:23:18 +0700

    Update puann.py

--- puann.py
+++ puann.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import streamlit as st
  
 # Header
-st.header('Nugroho :sparkles:')
+st.header('Puan :sparkles:')
 st.subheader('Plot')
  
 x = np.linspace(-2 * np.pi, 2 * np.pi, 1000)  # Generating x values from -2*pi to 2*pi
